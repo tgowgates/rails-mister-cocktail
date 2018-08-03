3e3b118772cbc7bc47bd88933dcbf4364abc4c86
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-03 11:17:16 +0100

    prepare for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+ingredient_hash = ingredients["drinks"]
+
+ingredient_hash.each do |ing_hash|
+  Ingredient.create(name: ing_hash["strIngredient1"])
+end
+
